e3f32fc6f4dbccabb598b5d248665c5a7432b88d
garrett armstrong <garrett.armstrong@gmail.com>
2015-09-08 08:42:25 -0400

    moved the NotePoint factory operations into MatrixBase.

--- matrixbase.py
+++ matrixbase.py
@@ -6,6 +6,7 @@ from kivy.app import App
 from kivy.graphics import Line, Color
 
 from notepoint import NotePoint
+from notepoint import NotePointLabel
 
 
 class MatrixBase(RelativeLayout):
@@ -35,6 +36,14 @@ class MatrixBase(RelativeLayout):
         'thirds_down':  0.8,
         'fifths_up':    1.5,
         'fifths_down':  2.0 / 3
+    }
+    relations_mult_addx_addy = {
+        'octaves_up':   [2.0,       0,  100],
+        'octaves_down': [0.5,       0, -100],
+        'thirds_up':    [1.25,   -105,   33.3],
+        'thirds_down':  [0.8,     105,  -33.3],
+        'fifths_up':    [1.5,      38,   58.3],
+        'fifths_down':  [2.0 / 3, -38,  -58.3]
     }
 
 
@@ -90,8 +99,9 @@ class MatrixBase(RelativeLayout):
 
     def make_first_notepoint(self, text):
         """
-        FundMatrix and MelodyMatrix must instantiate one NotePoint object each.  
-        Afterward, the *Matrix classes can 
+        FundMatrix and MelodyMatrix must instantiate one NotePoint object each.
+        Afterward, the *Matrix classes can build a network of NotePoints based on the
+        attributes of the original NotePoint.
         """
         a = NotePoint()
         a.text = text
@@ -100,15 +110,7 @@ class MatrixBase(RelativeLayout):
         a.factors_dict = {'octaves': 0, 'fifths': 0, 'thirds': 0}
         a.tonality = self.general_settings['scale']
         a.attach_label()
-
         self.add_widget(a)
-
-    def set_tonality(self):
-        """
-        When this class is inherited by MelodyMatrix, pass.
-        When it's inherited by FundMatrix, it's overloaded.
-        """
-        pass
 
     def populate_first_octave(self):
         '''
@@ -147,28 +149,6 @@ class MatrixBase(RelativeLayout):
             self.octaves_up = 2
             self.octaves_down = 1
 
-    def make_next_octaves(self):
-
-        settings_dict = self.which_class_settings_to_use()
-
-        for child in self.children:
-            self.first_octave_set.add(child)
-
-        for relation in ['octaves_up', 'octaves_down']:
-            count = 0
-            if int(settings_dict[relation]) > 0:
-                self.create_next_notepoint(relation)
-                count += 1
-            while count < int(settings_dict[relation]):
-                self.create_next_octave(relation)
-                count += 1
-
-    def which_class_settings_to_use(self):
-        if self.__class__.__name__ == 'FundMatrix':
-            return self.fund_settings
-        elif self.__class__.__name__ == 'MelodyMatrix':
-            return self.melody_settings
-
     def create_next_notepoint(self, relation):
         """
         Instructs self (E.g., FundMatrix or MelodyMatrix) to make a NotePoint one relation
@@ -177,25 +157,10 @@ class MatrixBase(RelativeLayout):
 
         multiplier = self.relations_multiplier[relation]
 
-        for child in self.children:
-            self.ratios_set.add(child.ratio)
-            if child.ratio * multiplier not in self.ratios_set:
-                child.make_related_note(relation)
-
-    def create_next_octave(self, relation):
-        """
-        Looks at a pseudo-registry in MelodyMatrix or FundMatrix, holding the NotePoints in the
-        first octave.  If there isn't an extant NotePoint at Original Notepoint * relation, it
-        creates one there.
-        """
-
-        self.temp_octave, self.next_octave_set = self.next_octave_set, set()
-        multiplier = self.relations_multiplier[relation]
-
-        for notepoint in self.temp_octave:
+        for notepoint in self.children:
             self.ratios_set.add(notepoint.ratio)
             if notepoint.ratio * multiplier not in self.ratios_set:
-                notepoint.make_related_note(relation)
+                self.make_related_note(notepoint, relation)
 
     def remove_top_third(self):
         """
@@ -223,6 +188,50 @@ class MatrixBase(RelativeLayout):
                     self.remove_widget(child)
                     self.ratios_set.remove(child.ratio)
 
+    def set_tonality(self):
+        """
+        When this class is inherited by MelodyMatrix, pass.
+        When it's inherited by FundMatrix, it's overloaded.
+        """
+        pass
+
+    def make_next_octaves(self):
+
+        settings_dict = self.which_class_settings_to_use()
+
+        for child in self.children:
+            self.first_octave_set.add(child)
+
+        for relation in ['octaves_up', 'octaves_down']:
+            count = 0
+            if int(settings_dict[relation]) > 0:
+                self.create_next_notepoint(relation)
+                count += 1
+            while count < int(settings_dict[relation]):
+                self.create_next_octave(relation)
+                count += 1
+
+    def which_class_settings_to_use(self):
+        if self.__class__.__name__ == 'FundMatrix':
+            return self.fund_settings
+        elif self.__class__.__name__ == 'MelodyMatrix':
+            return self.melody_settings
+
+    def create_next_octave(self, relation):
+        """
+        Looks at a pseudo-registry in MelodyMatrix or FundMatrix, holding the NotePoints in the
+        first octave.  If there isn't an extant NotePoint at Original Notepoint * relation, it
+        creates one there.
+        """
+
+        self.temp_octave, self.next_octave_set = self.next_octave_set, set()
+        multiplier = self.relations_multiplier[relation]
+
+        for notepoint in self.temp_octave:
+            self.ratios_set.add(notepoint.ratio)
+            if notepoint.ratio * multiplier not in self.ratios_set:
+                self.make_related_note(notepoint, relation)
+
     def add_lines(self):
         """
         Draws a line on self.canvas.before between any NotePoints related by a Perfect Fifth or
@@ -240,6 +249,84 @@ class MatrixBase(RelativeLayout):
                                 Color(0.6, 0.6, 0.6, 1)
                             Line(points=[notepoint_x.center[0], notepoint_x.center[1],
                                          notepoint_y.center[0], notepoint_y.center[1]], width=1.25)
+
+    def make_related_note(self, notepoint, relation):
+        """
+        Makes one new NotePoint object, using two arguments: 'notepoint' the referenced NotePoint
+        object, and 'relation' the relation between that NotePoint and the new Notepoint to be
+        made.  Ends after initializing a new NotePoint object, 'new_note' and setting all it's
+        attributes.
+        """
+        distance, direction = relation.split('_')
+        multiplier, move_x, move_y = self.relations_mult_addx_addy[relation]
+
+        new_note = NotePoint()
+        new_note.center = self.assign_new_note_center(notepoint, move_x, move_y)
+        new_note.ratio = self.assign_new_note_ratio(notepoint, multiplier)
+        new_note.factors_dict = self.assign_new_note_factors_dict(notepoint, distance, direction)
+        new_note.text = self.assign_new_note_text(notepoint, relation)
+        new_note.tonality = self.assign_new_note_tonality(notepoint, distance)
+        self.add_widget(new_note)
+        new_note.attach_label()
+        self.register_new_note(new_note, distance)
+
+    @staticmethod
+    def assign_new_note_center(notepoint, move_x, move_y):
+        new_center = [notepoint.center_x + move_x,
+                      notepoint.center_y + move_y]
+        return new_center
+
+    @staticmethod
+    def assign_new_note_ratio(notepoint, multiplier):
+        new_ratio = notepoint.ratio * multiplier
+        return new_ratio
+
+    @staticmethod
+    def assign_new_note_factors_dict(notepoint, distance, direction):
+        new_factors_dict = dict(notepoint.factors_dict)
+        if direction == 'up':
+            new_factors_dict[distance] += 1
+        elif direction == 'down':
+            new_factors_dict[distance] -= 1
+        return new_factors_dict
+
+    @staticmethod
+    def assign_new_note_text(notepoint, relation):
+        full_scale = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
+        relations_steps = {
+            'octaves_up': 12,
+            'octaves_down': -12,
+            'thirds_up': 4,
+            'thirds_down': -4,
+            'fifths_up': 7,
+            'fifths_down': -7
+        }
+        original_text_index = full_scale.index(notepoint.text)
+        shift_by = relations_steps[relation]
+        adjusted_note_index = original_text_index + shift_by
+        adjusted_note_index = adjusted_note_index % 12
+        return full_scale[adjusted_note_index]
+
+    @staticmethod
+    def assign_new_note_tonality(self, distance):
+        if distance == 'octaves':
+            return self.tonality
+        else:
+            pass
+
+    def attach_label(self):
+        l = NotePointLabel()
+        l.color = [1, .89, .355, 1]
+        l.size = self.size
+        l.center = self.center
+        l.text = self.text
+        l.font_size = 22
+        self.add_widget(l)
+
+    def register_new_note(self, new_note, distance):
+        if distance == 'octave':
+            self.next_octave_set.add(new_note)
+        self.ratios_set.add(new_note.ratio)
 
     def update_globals_from_last_fund_notepoint(self, notepoint):
         '''
